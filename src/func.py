4798008642bbd3796535f1de43549f68a3170b98
zixeel <strpan549@gmail.com>
2023-08-12 19:42:39 +0300

    add sort func

--- src/func.py
+++ src/func.py
@@ -16,3 +16,33 @@ def get_executed_list(operation_list):
         if operation.get('state') and operation['state'] == 'EXECUTED':
             all_executed_op.append(operation)
     return all_executed_op
+
+
+# def get_date(operation):
+# """"ДЛЯ ПОНИМАНИЯ"""
+#     return operation['date']
+
+
+def sort_op_list(op_list):
+    return sorted(op_list, key=lambda operation: operation['date'], reverse=True)[:5]
+
+
+
+
+
+
+
+
+
+
+
+
+# data = load_operations()
+# data_ex = get_executed_list(data)
+# print(sort_op_list(data_ex))
+
+
+# def sut(x):
+#     return x*5
+# lambda x: x*5
+
