07f2baeffebf1d1aa75e5c34736806ff2ffc1a88
zixeel <strpan549@gmail.com>
2023-08-15 14:58:38 +0300

    add hide nums func

--- src/func.py
+++ src/func.py
@@ -24,25 +24,34 @@ def get_executed_list(operation_list):
 
 
 def sort_op_list(op_list):
+    """Принимает список екзекьютед операций сортирует их по дате и выводит последние 5"""
     return sorted(op_list, key=lambda operation: operation['date'], reverse=True)[:5]
 
 
+def hide_nums(number):
+    """Принимает номер счёт отправителя определяет какая это карта или счёт и москирует данные на выходе в соответсвии с требованиями курсовой"""
+    if number.startswith('Счет'):
+        return '**'+number[-4:]
+    elif number.startswith('Visa Platinum'):
+        number = number.replace('Visa Platinum ', '')
+        return 'Visa Platinum' + ' ' + number[0:4] + " " + number[4:6] + '**' + ' ' + '****' + ' ' + number[-4:]
+    elif number.startswith('Visa Classic'):
+        number = number.replace('Visa Classic ', '')
+        return 'Visa Classic' + ' ' + number[0:4] + " " + number[4:6] + '**' + ' ' + '****' + ' ' + number[-4:]
+    elif number.startswith('MasterCard'):
+        number = number.replace('MasterCard ', '')
+        return 'MasterCard' + ' ' + number[0:4] + " " + number[4:6] + '**' + ' ' + '****' + ' ' + number[-4:]
+    elif number.startswith('Maestro'):
+        number = number.replace('Maestro ', '')
+        return 'Maestro' + ' ' + number[0:4] + " " + number[4:6] + '**' + ' ' + '****' + ' ' + number[-4:]
+    elif number.startswith('Visa Gold'):
+        number = number.replace('Visa Gold ', '')
+        return 'Visa Gold' + ' ' + number[0:4] + " " + number[4:6] + '**' + ' ' + '****' + ' ' + number[-4:]
+    elif number.startswith('МИР'):
+        number = number.replace('МИР ', '')
+        return 'МИР' + ' ' + number[0:4] + " " + number[4:6] + '**' + ' ' + '****' + ' ' + number[-4:]
 
 
 
 
-
-
-
-
-
-
-# data = load_operations()
-# data_ex = get_executed_list(data)
-# print(sort_op_list(data_ex))
-
-
-# def sut(x):
-#     return x*5
-# lambda x: x*5
 
